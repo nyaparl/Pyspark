a94b85e3cf2ecffda3978558ea93d53709042075
nyaparl <sparkde99@gmail.com>
2024-02-23 21:53:11 +0530

    First Commit

--- practice/test.py
+++ practice/test.py
@@ -10,12 +10,3 @@ df = spark.read.format('csv').option('header','true') \
                              .load('D:/Data/test123.csv')
 df.show()
 
-# dept = [("Finance",10),
-#         ("Marketing",20),
-#         ("Sales",30),
-#         ("IT",40) ]
-#
-# deptColumns = ["dept_name","dept_id"]
-# deptDF = spark.createDataFrame(data=dept, schema = deptColumns)
-# deptDF.printSchema()
-# deptDF.show(truncate=False)
